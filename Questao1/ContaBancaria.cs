acfcca642b283489b4cd70033dbfca14bf815c8f
andresombra <andresombra.net@gmail.com>
2025-06-17 18:37:11 -0300

    Atualizacao e implementado propriedades

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria {
+        private const double TAXA_SAQUE = 3.50;
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        // Construtor com depósito inicial
+        public ContaBancaria(int numero, string titular, double depositoInicial)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = depositoInicial;
+        }
+
+        // Construtor sem depósito inicial
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+    }
+}
