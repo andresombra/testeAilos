49030ffcfb26209b04959e04952e90b11d66d57b
andresombra <andresombra.net@gmail.com>
2025-06-18 18:48:53 -0300

    Incluido Entities

---
+++ Questao5/Infrastructure/Services/Controllers/ContaCorrenteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace Questao5.Infrastructure.Services.Controllers
+{
+    public class ContaCorrenteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
